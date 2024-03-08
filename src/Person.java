e8b4941fdbc72016f8c3e44f436a8c37c753a2d0
aashif-sajah <masaashifsajah@gmail.com>
2024-03-08 22:54:59 +0530

    Add Person class
    Created a new class named Person with attributes for name, surname, and email. Implemented constructor, getter and setter methods for these attributes, and a method to display the information of a person.

---
+++ src/Person.java
@@ -0,0 +1,45 @@
+public class Person {
+    private String name;
+    private String sureName;
+    private String email;
+
+    public Person(String objName, String objSureName, String objEmail) {
+        name = objName;
+        sureName = objSureName;
+        email = objEmail;
+    }
+
+    public void setName(String objName) {
+        name = objName;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSureName(String objSureName) {
+        name = objSureName;
+
+    }
+
+    public String getSureName() {
+        return sureName;
+    }
+
+    public void setEmail(String email) {
+        name = email;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void displayPersonInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + sureName);
+        System.out.println("Email: " + email);
+    }
+
+}
